46440bda58f6d8d935ebd7b0e9794959cf0a4d89
Vikram Jha <77636318+Vikram-Jha@users.noreply.github.com>
2023-06-06 00:20:22 +0530

    Update 010_Find_the_Duplicate_Number.java

--- 010_Find_the_Duplicate_Number.java
+++ 010_Find_the_Duplicate_Number.java
@@ -20,3 +20,29 @@ public class Solution{
     }
 }
 
+// Better: TC: 0(N) & SC: O(N)
+
+/*
+  1. Use hashing. Since we know value of arr[i] in the range [0,N-1]. So, declare a new array temp of size n.
+  2. for each occurance of an element increase the index of that element by one.
+  3. traverse through temp array if temp[i] > 1 then that's repeating element return that element.
+*/
+
+public class Solution{
+    public static int findDuplicate(ArrayList<Integer> arr, int n){
+        int temp[] = new int[n];
+      
+        for(int i=0; i<n; i++){
+            temp[arr.get(i)]++;
+        }
+      
+        int reqNo =0;
+      
+        for(int i=0; i<n; i++){
+            if(temp[i] > 1){
+                reqNo = i;
+            }
+        }
+        return reqNo;
+    }
+}
