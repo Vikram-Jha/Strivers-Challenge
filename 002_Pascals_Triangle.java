370ca27578d2d7eeebec908db52e6a68dbc5713c
Vikram Jha <77636318+Vikram-Jha@users.noreply.github.com>
2023-06-06 17:24:19 +0530

    Update 002_Pascals_Triangle.java

--- 002_Pascals_Triangle.java
+++ 002_Pascals_Triangle.java
@@ -30,8 +30,7 @@
         return res;
     }
 
-//  2. (i) Naive way is to calculate each element of an row using formula and print the row.
-//          Since each row has exactly n elements --> Iterate from 1 to n and calculate element using n-1Cc-1. ex for 5th row 4C0 4C1 4C2 4C3 4C4
+//  2. Since each row has exactly n elements --> Iterate from 1 to n and calculate element using n-1Cc-1. ex for 5th row 4C0 4C1 4C2 4C3 4C4
 
     public static ArrayList<Long> nthRowArr(int n) {
       ArrayList<Long> nthRow = new ArrayList<Long>();
@@ -42,4 +41,60 @@
       return nthRow;
     }
 
-//    (ii)
+//  3. There can be two approach to solve the problem: 1. Formula Based 2. Intuition based
+
+//1st Approach is to get all the element of pascal triangle using the n-1Cr-1 formula: TC: O(N^2 * R) & SC: O(N^2)
+
+public class Solution {
+	public static ArrayList<ArrayList<Long>> printPascal(int n) {
+        ArrayList<ArrayList<Long>> sol = new ArrayList<>();
+
+        for (int row = 1; row <= n; row++) {                
+            ArrayList<Long> temp = new ArrayList<>(); 
+            for (int col = 1; col <= row; col++) {            // The no. of elements in a row  in a pascal triangle is equals to the row number.
+                temp.add(nCr(row - 1, col - 1));
+            }
+            sol.add(tempLst);
+        }
+        return sol;
+	}
+
+	public static long nCr(int n, int r) {
+        long res = 1;
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return res;
+    }
+}
+
+// 2nd Approach is to optimise the formula.
+
+// Lets observe: 1 4 6 4 1 this was our fifth row(1 index as first). See pattern  1, 4/1, (4 * 3)/(1 *2), (4*3*2)/(1*2*3), (4*3*2*1)/(1*2*3*4).
+// If we take zero base indexing then ans =1 in each iteration ans = ans * (rows - columns) / columns if we follow zero based indexing.
+// So instead of using a nCr function we can append a temp list in each iteration based on this formula and get each row.
+
+public class Solution {
+	public static ArrayList<ArrayList<Long>> printPascal(int n) {
+        ArrayList<ArrayList<Long>> ans = new ArrayList<>();
+
+        for (int row = 1; row <= n; row++){
+            ans.add(getRow(row));
+        }
+        return ans;
+	}
+
+	public static ArrayList<Long> getRow(int row) {
+        ArrayList<Long> temp = new ArrayList<>();
+		temp.add(1L);
+
+		long ans = 1;
+        for (int col = 1; col <row ; col++) {
+            ans = ans * (row - col);
+			ans = ans / col;
+			temp.add(ans);
+        }
+        return temp;
+    }
+}
