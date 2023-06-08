f3355828de28d867276af7541d2dc0ce95283d50
Vikram Jha <77636318+Vikram-Jha@users.noreply.github.com>
2023-06-09 00:24:22 +0530

    Update 017_Power_(x,n).java

--- 017_Power_(x,n).java
+++ 017_Power_(x,n).java
@@ -48,3 +48,18 @@ class Solution {
         return n<0 ? (double)(1)/(double)(ans) : ans; 
     }
 }
+
+// Modular Exponential Question CN:
+ public static int modularExponentiation(int x, int n, int m) {
+        long ans = 1;
+        long xx = x;
+
+        while(n>0){    
+            if(n%2 !=0){
+                ans = ((ans)%m *(xx)% m ) %m;          
+            }
+            xx = ((xx)%m *(xx)%m)%m;
+            n= n>>1;
+        }  
+        return(int) (ans%m);
+    }
